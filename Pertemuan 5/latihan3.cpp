a801e8ed0dfa1bd3cf2c741a1eab58ef26a5db92
aiylanazneenh-hub <aiylanazneenh@gmail.com>
2026-03-18 15:35:33 +0700

    Pertemuan 5 : latihan

---
+++ Pertemuan 5/latihan3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"masukkan bilangan : "; cin>>x;
+
+    if (x>5 && x<8){
+        cout<<"A";
+    }
+    else if (x>5){
+        cout<<"B";
+    }
+    else{
+        cout<<"C";
+    }
+
+}
---
+++ Pertemuan 5/latihan5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"input a : ";cin>>a;
+    cout<<"input b : ";cin>>b;
+
+    if (a<=b && b>2){
+        cout<<"YES";
+    }
+    else {
+        cout<<"NO";
+    }
+}
